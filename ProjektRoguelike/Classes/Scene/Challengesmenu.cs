ec4727ab37438eb51a7b92996fa23febb416adde
Luca Traeg <239trlu@bbs-ww.de>
2020-08-24 13:18:26 +0200

    Added Challengesmenu and Statsmenu Added Trapdoor Added some Globals and UI work Added Art for Challenges and Stats

--- ProjektRoguelike/Classes/Scene/Challengesmenu.cs
+++ ProjektRoguelike/Classes/Scene/Challengesmenu.cs
@@ -9,86 +9,38 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ProjektRoguelike
 {
-    public class Optionsmenu : Scene
+    public class Challengesmenu : Scene
     {
         Texture2D bkg;
 
-        private int fullscreen, music, sfx;
-
-        OptionsButton FullScreen, MusicVolume, SFXVolume;
-
-        List<OptionsButton> buttons = new List<OptionsButton>();
-
-        PassObject saveObject;
-        Button Savebutton;
+        //List<OptionsButton> buttons = new List<OptionsButton>();
 
         XDocument xml;
 
-        public Optionsmenu()
+        public Challengesmenu()
         {
-            bkg = Globals.Content.Load<Texture2D>("Sprites/Misc/Options");
+            bkg = Globals.Content.Load<Texture2D>("Sprites/Misc/Challenges");
 
             xml = Globals.save.GetFile("xml\\options.xml");
 
-            saveObject = SaveOptions;
-            Savebutton = new Button(Button.ButtonState.Selected, saveObject, null, Camera.Position + new Vector2(400, 520));
-
-            FullScreen = new OptionsButton("Full Screen", fullscreen, 1, Button.ButtonState.Selected, Camera.Position + new Vector2(400, 220));
-            buttons.Add(FullScreen);
-
-            MusicVolume = new OptionsButton("Music Volume", music, 20, Button.ButtonState.Unselected, Camera.Position + new Vector2(400, 320));
-            buttons.Add(MusicVolume);
-
-            SFXVolume = new OptionsButton("SFX Volume", sfx, 20, Button.ButtonState.Unselected, Camera.Position + new Vector2(400, 420));
-            buttons.Add(SFXVolume);
-
-            LoadData(xml);
+            //LoadData(xml);
         }
 
         public override void Update()
         {
-            Savebutton.Update();
-            // Update the buttons and check which button is supposed to be selected.
-            for (int i = 0; i < buttons.Count; i++)
-            {
-                buttons[i].Update();
-                if (buttons[i].buttonState == Button.ButtonState.Selected && (Globals.GetKeyUp(Keys.W) || Globals.GetKeyUp(Keys.Up)) && i > 0)
-                {
-                    buttons[i - 1].buttonState = Button.ButtonState.Selected;
-                    buttons[i].buttonState = Button.ButtonState.Unselected;
-
-                    // Break because the player can move the arrow only once per frame.
-                    break;
-                }
-                if (buttons[i].buttonState == Button.ButtonState.Selected && (Globals.GetKeyUp(Keys.S) || Globals.GetKeyUp(Keys.Down)) && i < buttons.Count - 1)
-                {
-                    buttons[i + 1].buttonState = Button.ButtonState.Selected;
-                    buttons[i].buttonState = Button.ButtonState.Unselected;
-
-                    // Break because the player can move the arrow only once per frame.
-                    break;
-                }
-            }
-
             // Switch back to the mainmenu by hitting the escape key. 
             if (Globals.GetKeyUp(Microsoft.Xna.Framework.Input.Keys.Escape))
             {
-                //SaveOptions(null);
                 Globals.gamestate = Gamestate.Mainmenu;
             }
         }
 
         public override void Draw()
         {
-            Savebutton.Draw();
-            for (int i = 0; i < buttons.Count; i++)
-            {
-                buttons[i].Draw();
-            }
             Globals.SpriteBatch.Draw(bkg, new Rectangle(Camera.Position.ToPoint(), (Globals.WindowDimensions + Vector2.One).ToPoint()), null, Color.White, 0, new Vector2(0f), SpriteEffects.None, 0.1f);
-            Globals.SpriteBatch.DrawString(Globals.Content.Load<SpriteFont>("Fonts/Consolas24"), "Press enter to save", Savebutton.position + new Vector2(75, 15), Color.White);
         }
 
+        /*
         public void SaveOptions(object info)
         {
             XDocument xml = new XDocument(new XElement("Root",
@@ -133,5 +85,6 @@ namespace ProjektRoguelike
                 }
             }
         }
+        */
     }
 }
